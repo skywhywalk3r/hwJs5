377bab49b0319128406d2c626465fb5e182ee47e
Gleb <mgleb830@gmail.com>
2026-05-31 18:29:17 +0300

    done

---
+++ app.js
@@ -0,0 +1,86 @@
+const drink = "Caffee";
+switch (drink) {
+  case "Caffee":
+    console.log("You choose caffee. Good morning");
+    break;
+
+  case "Tea":
+    console.log("You choose tea. It's more about relax");
+    break;
+
+  case "Juice":
+    console.log("You choose good vitamins");
+    break;
+  default:
+    console.log(
+      "Undefined drink, please choose another drink Caffee, Tea or Juice.",
+    );
+}
+
+const dayOfWeek = "Субота".toLowerCase();
+
+if (
+  dayOfWeek === "Понеділок" ||
+  dayOfWeek === "Вівторок" ||
+  dayOfWeek === "Середа" ||
+  dayOfWeek === "Четвер" ||
+  dayOfWeek === "П'ятниця"
+) {
+  console.log("Це робочий день. Час до роботи або навчання!");
+} else if (dayOfWeek === "Субота" || dayOfWeek === "Неділя") {
+  console.log("Це вихідний день! Відпочивайте.");
+} else {
+  console.log("Некоректна назва дня тижня.");
+}
+
+
+const monthNumber = 5;
+
+if (monthNumber === 12 || monthNumber === 1 || monthNumber ===2) {
+    console.log("Пора року - Зима");
+} else if (monthNumber >= 3 && monthNumber <= 5) {
+    console.log("Пора року - Весна");
+} else if (monthNumber >= 6 && monthNumber <=8) {
+    console.log("Пора року - Літо");
+} else if (monthNumber >= 9 && monthNumber <= 11) {
+    console.log("Пора року - Осінь");
+} else {
+    console.log("Неіснуючий номер місяця, вивберіть від 1 до 12.");
+}
+
+const color = "green"
+
+switch (color.toLocaleLowerCase()) {
+    case "red": 
+    console.log("Stop!");
+    break;
+
+    case "green":
+    console.log("You can go");
+    break;
+
+    case "yellow":
+    console.log("You need to wait");
+    break;
+    default:
+        console.log("It's broken")
+}
+
+const num1 = 10;
+const num2 = 0;
+const operator = "/"; // "+", "-", "*", "/"
+
+
+if (operator === "/" && num2 === 0) {
+    console.warn("Warning you can't decrease 0!");
+} else {
+    let result;
+    if (operator ==="+")result = num1 + num2;
+    if (operator === "-") result = num1 - num2;
+    if (operator === "*") result = num1 * num2;
+    if (operator === "/") result = num1 / num2;
+
+
+    console.log("Result:", result);
+}
+
